002c4da38e3b6c93af36697ad26e34a572e1cec7
Brues <5278969+brues-code@users.noreply.github.com>
2026-05-15 14:34:25 -0500

    update comment

--- AddOns/!!!ClassicAPI/Util/AddOnCompat.lua
+++ AddOns/!!!ClassicAPI/Util/AddOnCompat.lua
@@ -1,6 +1,6 @@
 if C_AddOns.DoesAddOnExist('pfUI') then
     EventUtil.ContinueOnAddOnLoaded('pfUI', function()
-        -- pfUI redeclares the RAID_CLASS_COLORS object at least 14 times (insane!!)
+        -- pfUI\pfUI.lua redeclares the RAID_CLASS_COLORS object at least 14 times (insane!!)
         -- This stops it from replacing all our ColorMixin values
         pfUI.UpdateColors = function() end
         RAID_CLASS_COLORS = setmetatable(RAID_CLASS_COLORS, { __index = function()
